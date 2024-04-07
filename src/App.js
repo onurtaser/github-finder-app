b468ca1089d804a83914f276bf3c59a6d1749ffc
onurtaser <onur.taser@outlook.com>
2024-04-07 23:43:50 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./component/layout/Navbar";
+import Footer from "./component/layout/Footer";
+import Home from "./component/layout/Home";
+import { BrowserRouter as Route, Router, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+
+        <main>Content</main>
+      </div>
+    </>
   );
 }
 
