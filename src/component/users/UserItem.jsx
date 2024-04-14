1f5de5b977c84af36b2720bb119ddfe81dad0529
onurtaser <onur.taser@outlook.com>
2024-04-15 00:53:19 +0300

    users added to home page

---
+++ src/component/users/UserItem.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function UserItem({user: {login, avatar_url}}) {
+  return (
+    <div className='card bg-base-100 shadow-lg'>
+        <div className="card-body flex-row items-center space-x-4">
+            <div className="avatar">
+                <div className="w-24 rounded">
+                    <img src={avatar_url} alt="" />
+                </div>
+            </div>
+            <div>
+                <h2 className="card-title">{login}</h2>
+                <Link className='text-base-content text-opacity-40' to={`/users/${login}`}>Visit Profile</Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default UserItem
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import UserList from '../component/users/UserList'
 
 function Home() {
   return (
     <div>
-      <h1 className="text-9xl text-white">Welcome</h1>
+      {/* <h1 className="text-9xl text-white">Welcome</h1> */}
+      <UserList />
     </div>
   )
 }
